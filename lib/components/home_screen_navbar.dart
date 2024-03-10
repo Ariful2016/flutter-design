79dfacba632d663f047b5f14b078b142144384bb
Ariful <arifewucse2016@gmail.com>
2024-03-10 16:00:52 +0600

    initial setup

---
+++ lib/components/home_screen_navbar.dart
@@ -0,0 +1,46 @@
+
+import 'package:design_with_firebase/components/searchfield_widget.dart';
+import 'package:design_with_firebase/components/sidebar_button.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../screens/profile_screen.dart';
+
+class HomeScreenNavBar extends StatelessWidget {
+  HomeScreenNavBar({required this.triggerAnimation});
+
+  final VoidCallback triggerAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SidebarButton(
+            triggerAnimation: triggerAnimation,
+          ),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ProfileScreen()));
+            },
+            child: CircleAvatar(
+              radius: 18.0,
+              backgroundImage: AssetImage('asset/images/profile.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
